0b1575e73be2c70d799b3756ffffcfc001fde5a6
Nnorom Ifeanyi Paul <nnoromifeanyi@gmail.com>
2020-04-14 20:52:32 +0100

    estimator + API first commit

---
+++ src/api.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify, make_response
+from flask_restful import Api, Resource, reqparse
+from simplexml import dumps
+import json
+
+from estimator import estimator
+
+app = Flask(__name__)
+api = Api(app, default_mediatype=None)
+
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+    resp = make_response(json.dumps(data), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+def output_xml(data, code, headers=None):
+    """Make a Flask response with a XML encoded body"""
+    resp = make_response(dumps({'response': data}), code)
+    resp.headers.extend(headers or {})
+
+    return resp
+
+class Covid19EstimatorApi(Resource):
+    def __init__(self, representations=None):
+        self.representations = representations
+        super(Covid19EstimatorApi, self).__init__()
+    
+    def post(self):
+        parser = reqparse.RequestParser(bundle_errors=True)
+        
+        parser.add_argument('periodType', type=str, required=True, location='form')
+        parser.add_argument('timeToElapse', type=int, required=True, location='form')
+        parser.add_argument('reportedCases', type=int, required=True, location='form')
+        parser.add_argument('population', type=int, required=True, location='form')
+        parser.add_argument('totalHospitalBeds', type=int, required=True, location='form')
+        
+        args = parser.parse_args(strict=True)
+        
+        output = estimator(args) 
+
+        return output
+
+api.add_resource(Covid19EstimatorApi, '/api/v1/on-covid-19')
+api.add_resource(Covid19EstimatorApi, '/api/v1/on-covid-19/json', 
+                                resource_class_kwargs={'representations': {'application/json': output_json}},
+                                endpoint='covid19_estimator_api_json'
+                            )
+api.add_resource(Covid19EstimatorApi, '/api/v1/on-covid-19/xml', 
+                                resource_class_kwargs={'representations': {'application/xml': output_xml}},
+                                endpoint='covid19_estimator_api_xml'
+                            )
+
+app.run(debug=True)
--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,69 @@
 def estimator(data):
+  # Append regional data to input
+  regionData = dict()
+  regionData['name'] = 'Africa'
+  regionData['avgAge'] = 19.7
+  regionData['avgDailyIncomeInUSD'] = 5
+  regionData['avgDailyIncomePopulation'] = 0.71
+
+  data['region'] = regionData
+ 
+  #setup input parameters
+  periodType = str(data['periodType'])
+  timeToElapse = int(data['timeToElapse'])
+  reportedCases = int(data['reportedCases'])
+  population = int(data['population'])
+  totalHospitalBeds = int(data['totalHospitalBeds'])
+
+  avgDailyIncomeInUSD = data['region']['avgDailyIncomeInUSD']
+  avgDailyIncomePopulation = data['region']['avgDailyIncomePopulation']
+
+  
+  # setup output parameters
+  impact = severImpact = dict()
+  
+  # estimates duration in days
+  if periodType == 'months':
+    noOfDays = timeToElapse*30
+  elif periodType == 'weeks':
+    noOfDays = timeToElapse*7
+  else:
+    noOfDays = timeToElapse
+
+  impact['currentlyInfected'] = reportedCases*10
+  severImpact['currentlyInfected'] = reportedCases*50
+
+  # factor for number of infected people in a given day
+  factor = int(noOfDays/3)
+
+  impact['infectionsByRequestedTime'] = int(impact['currentlyInfected']*pow(2, factor))
+  severImpact['infectionsByRequestedTime'] = int(severImpact['currentlyInfected']*pow(2, factor))
+
+  # estimated number of severe positive cases
+  impact['severeCasesByRequestedTime'] = int(0.15*impact['infectionsByRequestedTime'])
+  severImpact['severeCasesByRequestedTime'] = int(0.15*severImpact['infectionsByRequestedTime'])
+
+  # estimated number of available hospital beds for severe COVID-19 positive patients.
+  impact['hospitalBedsByRequestedTime'] = totalHospitalBeds - impact['severeCasesByRequestedTime']
+  severImpact['hospitalBedsByRequestedTime'] = totalHospitalBeds - severImpact['severeCasesByRequestedTime']
+
+  # estimated number of severe positive cases that will require ICU care.
+  impact['casesForICUByRequestedTime'] = int(0.05*impact['infectionsByRequestedTime'])
+  severImpact['casesForICUByRequestedTime'] = int(0.05*severImpact['infectionsByRequestedTime'])
+
+  # estimated number of severe positive cases that will require ventilators.
+  impact['casesForVentilatorsByRequestedTime'] = int(0.02*impact['infectionsByRequestedTime'])
+  severImpact['casesForVentilatorsByRequestedTime'] = int(0.02*severImpact['infectionsByRequestedTime'])
+
+  # compute the average daily for specified period
+  impact['dollarsInFlight'] = int((impact['infectionsByRequestedTime']*avgDailyIncomePopulation*avgDailyIncomeInUSD)/noOfDays)
+  severImpact['dollarsInFlight'] = int((severImpact['infectionsByRequestedTime']*avgDailyIncomePopulation*avgDailyIncomeInUSD)/noOfDays)
+
+  # Output data
+  data = {
+    'data': data,
+    'impact': impact,
+    'severImpact': severImpact
+  }
+
   return data
